09d219f1c3e07f1ff147fd7536867a21db875ee0
fballest42 <ballesteros.fdo@gmail.com>
2022-10-18 09:23:09 +0200

    Error during buiding

--- inception/srcs/requirements/wordpress/tools/wpsetup.sh
+++ inception/srcs/requirements/wordpress/tools/wpsetup.sh
@@ -8,7 +8,7 @@ mv wp-cli.phar /usr/local/bin/wp;
 cd /var/www/wordpress
 
 wp config create --allow-root --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASS} --dbhost='mariadb' --dbcharset='utf8' --locale='es_ES' --path=/var/www/wordpress
-wp core install --allow-root --url=${HTTP_NAME} --title="FBALLEST WORDPRESS HOMEPAGE" --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASS} --admin_email=${EMAIL_ADMIN} --path=/var/www/wordpress
+wp core install --allow-root --url=${HTTP_NAME} --title="FBALLEST WORDPRESS HOMEPAGE" --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASS} --admin_email=${EMAIL_ADMIN} --skip-email --path=/var/www/wordpress
 wp user create --allow-root ${WP_USER_NAME} ${EMAIL_USER} --user_pass=${WP_USER_PASS} --role=editor --path=/var/www/wordpress
 
 echo "+-----------------------------------------------------------------+"
